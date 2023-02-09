3010ef15694431ff520a06282ed8bf8719b2fb19
AlexVoiv <118457797+AlexVoiv@users.noreply.github.com>
2023-02-09 19:05:46 +0200

    update

---
+++ newJavaProject/src/practice__ninefeb.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class practice__ninefeb {
+    public static void main(String[] args) {
+        int[] numbers = { 2, 3, 5, 1, 4 };
+        Arrays.sort (numbers);
+        System.out.println(numbers.length);
+    }
+
+}
